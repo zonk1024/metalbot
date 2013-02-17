dedec85e25dd02108620ee8de04647353f249d18
Andy Schmitt <andy@parthenonsoftware.com>
2013-02-17 00:49:55 -0800

    Added a series of refactors, most of which were made during PDX Python.

--- metalbot.py
+++ metalbot.py
@@ -28,16 +28,21 @@ class MetalBot(botlib.Bot):
         self._requeue()
 
     def initialize_db(self):
+        print "Start initialize of DB...getting songs"
         songs = self.mpc.listallinfo()
         cur = self.db.cursor()
+        print "Start iteration..."
+        # This iteration blows, rewrite.
         for song in songs:
-            if "file" in song:
+            if "file" in song and "title" in song:
                 cur.execute("SELECT COUNT(*) FROM songlist WHERE filename=?", (song["file"], ))
                 if cur.fetchone()[0] == 0:
                     print "Inserting %s" % song["file"]
                     cur.execute("INSERT INTO songlist (filename, artist, album, title) VALUES (?,?,?,?)", \
                             (song["file"], song["artist"], song["album"], song["title"]))
+        print "Commit all that stuff"
         self.db.commit()
+        print "End initialize of DB"
         
 
     def _reconnect(self):
@@ -61,17 +66,22 @@ class MetalBot(botlib.Bot):
  
         if botlib.check_found(self.data, "!metalbot"):
             if self._process_cmd(self.data):
-                fn = getattr(self, self.command + "_action")
                 try:
                     self.mpc.status()
                 except ConnectionError:
                     self._reconnect()
 
                 try:
+                    fn = getattr(self, self.command + "_action")
                     fn(self.args)
                 except AttributeError:
                     self.protocol.privmsg(self.channel, "Sorry, '{0}' means nothing to me".format(self.command))
 
+    def _getsongid(self, filename):
+        db = sqlite3.connect(DB)
+        cur = db.cursor()
+        cur.execute("SELECT id FROM songlist WHERE filename=?", (filename,))
+        return cur.fetchone()[0]
 
     def hello_action(self, args):
         self.protocol.privmsg(self.channel, "Hello {0}!".format(self.username))
@@ -82,15 +92,19 @@ class MetalBot(botlib.Bot):
             self.protocol.privmsg(self.channel, "Nothing's playing at the moment")
         else:
             s = self.mpc.currentsong()
-            cur = self.db.cursor()
-            cur.execute("SELECT id FROM songlist WHERE filename=?", (s["file"],))
-            sid = cur.fetchone()[0]
-
+            sid = self._getsongid(s["file"])
             self.protocol.privmsg(self.channel, "Now playing [{0}]: {1} - {2}".format(sid, s["artist"], s["title"]))
 
     def next_action(self, args):
-        s = self.mpc.next()
-        self.playing_action(args)
+        s = self.player_status
+        if "nextsong" in s:
+            # This is inefficient, but it would be a PITA to do two calls, one to get the filename,
+            # strip, search by filename for the song, pull info, &c.
+            s = self.mpc.playlistinfo()[int(s["nextsong"])]
+            sid = self._getsongid(s["file"])
+            self.protocol.privmsg(self.channel, "Next up [{0}]: {1} - {2}".format(sid, s["artist"], s["title"]))
+        else:
+            self.protocol.privmsg(self.channel, "Nothing appears to be up next")
 
     def downvote_action(self, args):
         self._vote(args, -1)
@@ -114,13 +128,15 @@ class MetalBot(botlib.Bot):
             return
 
         tag = args[0]
+        if not any(tag in s for s in ["artist", "album", "song", "any"]):
+            return
+
         tofind = " ".join(args[1:])
         songs = self.mpc.search(tag, tofind)
         cur = self.db.cursor()
         i = 0
         for s in songs:
-            cur.execute("SELECT id FROM songlist WHERE filename=?", (s["file"],))
-            sid = cur.fetchone()[0]
+            sid = self._getsongid(s["file"])
             self.protocol.privmsg(self.username, "[{0}]: {1} - {2} - {3}".format(unicode(sid), s["artist"], s["album"], s["title"]))
             if i > 30:
                 return
@@ -172,13 +188,11 @@ class MetalBot(botlib.Bot):
     # I make a new DB connection every time because you can't reuse the same one in multiple threads
     def _update_status(self):
         db = sqlite3.connect(DB)
+        cur = db.cursor()
         self.player_status = self.mpc.status()
         if self.player_status["state"] == "play":
             s = self.mpc.currentsong()
-            cur = db.cursor()
-            cur.execute("SELECT id FROM songlist WHERE filename=?", (s["file"],))
-            self.currentsong = cur.fetchone()[0] 
-            #print "Music status is %s and currentsong = [%d] %s" % (self.player_status["state"], self.currentsong, s["file"])
+            self.currentsong = self._getsongid(s["file"])
 
             cur.execute("DELETE FROM queue WHERE songid = %d" % self.currentsong)
             db.commit()
