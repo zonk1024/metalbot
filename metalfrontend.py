3bc9ea310c6f047c6b27bdd3fef821093d643aed
Andy Schmitt <andy@parthenonsoftware.com>
2013-02-24 23:36:16 -0800

    Added front end website updates allowing cover to be displayed.

--- metalfrontend.py
+++ metalfrontend.py
@@ -1,5 +1,6 @@
+import os, re
 import sqlite3
-from bottle import route, run, debug, request, validate, static_file, error
+from bottle import route, run, debug, request, validate, static_file, error, abort
 from bottle import jinja2_view as view, jinja2_template as template
 import mpd
 from metalbot import MetalBot
@@ -11,8 +12,11 @@ from bottle import default_app
 @route('/')
 @view('index.html')
 def main_page():
+    db = sqlite3.connect(settings.DB)
+    cur = db.cursor()
     mpc = mpd.MPDClient(use_unicode = True)
     mpc.connect(settings.MPD_SERVER, settings.MPD_PORT)
+
     s = mpc.status()
     nextsongs = []
     if "nextsong" in s:
@@ -23,10 +27,10 @@ def main_page():
 
     if s["state"] == "play":
         nowplaying = mpc.currentsong()
-        nowplaying["sid"] = MetalBot.getsongid(song["file"])
+        nowplaying["sid"] = MetalBot.getsongid(nowplaying["file"])
+        if os.path.isfile(os.path.join(settings.MPD_SOURCE, os.path.dirname(nowplaying["file"]), "cover.jpg")):
+            nowplaying["coverpath"] = u"/covers/{0}/cover.jpg".format(os.path.dirname(nowplaying["file"]))
 
-    db = sqlite3.connect(settings.DB)
-    cur = db.cursor()
     cur.execute("SELECT filename, title, artist FROM queue INNER JOIN songlist ON songlist.id=queue.songid ORDER BY queue.id")
     queuerows = cur.fetchall()
     queue = []
@@ -39,5 +43,11 @@ def main_page():
 
     return dict(nextup = nextsongs, nowplaying = nowplaying, queue = queue)
 
+@route("/covers/<coverpath:path>")
+def covers(coverpath):
+    if re.match(r".*cover\.jpg", coverpath) is None:
+        abort(404, "Cover file not found")
+
+    return static_file(coverpath, root=settings.MPD_SOURCE)
 
 run(host='localhost', port=8080)
--- settings.py
+++ settings.py
@@ -4,3 +4,4 @@ NICK = "Metalbot"
 DB = "bot.db"
 MPD_SERVER = "localhost"
 MPD_PORT = 6600
+MPD_SOURCE = "/home/aschmitt/Music"
